70d82fa998ce69f435ff3042415f879990e61371
sobeida <sobeidalagrange7@gmail.com>
2016-06-25 17:14:16 -0400

    Integrating API
    Retrieving the resources

--- js/app.js
+++ js/app.js
@@ -1,33 +1,44 @@
     $(document).ready(function () {
-        $('#searchform').bind('submit', function () {
-            api_key = "dk88st01cks0as9cv2iwr4hg";
-            terms = $('#access').val();
-            etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords=" +
-                terms + "&limit=12&includes=Images:1&api_key=" + api_key;
+        $('#searchform').submit(function (event) {
+            event.preventDefault();
+            var api_key = "dk88st01cks0as9cv2iwr4hg";
+            var terms = $('#access').val();
+            var etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords=" + terms + "&limit=12&includes=Images:1&api_key=" + api_key;
 
-            $('#result-container-right').empty();
-            $('<p></p>').text('Searching for ' + terms).appendTo('#result-container-right');
+            $('#result-container').empty();
+            $('<p></p>').text('Searching for ' + terms).appendTo('#result-container');
 
             $.ajax({
                 url: etsyURL,
                 dataType: 'jsonp',
                 success: function (data) {
+
                     if (data.ok) {
-                        $('#result-container-right').empty();
+                        $('.result-container ul').empty();
+
                         if (data.count > 0) {
-                            $.each(data.results, function (i, item) {
-                                $("<img/>").attr("src", item.Images[0].url_75x75).appendTo("#result-container-right").wrap(
-                                    "<a href='" + item.url + "'></a>"
-                                );
-                                if (i % 4 == 3) {
-                                    $('<br/>').appendTo('#result-container-right');
-                                }
+                            $.each(data.results, function (key, value) {
+
+                                console.log(value);
+
+                                var htmlOutput = "";
+
+                                htmlOutput += '<li>';
+                                htmlOutput += '<div class="card-image">';
+                                htmlOutput += '<h2 class="card-title">' + value.title + '</h2>';
+                                htmlOutput += '<p class="card-details card-description">' + value.description + '</p>';
+                                htmlOutput += '<p class="card-details">' + value.currency_code + ' ' + value.price + '</p>';
+                                htmlOutput += '<p class="card-details item-image"><a href="http://www.mrchocolate.com/"><img  width="100%" src="' + value.Images[0].url_fullxfull + '"></a></p>';
+                                htmlOutput += '</div>';
+                                htmlOutput += ' </li>';
+
+                                $('.result-container ul').append(htmlOutput);
                             });
                         } else {
-                            $('<p>No results.</p>').appendTo('#result-container-right');
+                            $('<p>No results.</p>').appendTo('#result-container');
                         }
                     } else {
-                        $('#result-container-right').empty();
+                        $('#result-container').empty();
                         alert(data.error);
                     }
                 }
